2f0d108006507f341a984d74a4cced88c114957b
Mordecai <edwincesaryauyosoto@gmail.com>
2019-09-15 19:08:59 -0500

    small add
    uu

---
+++ New Unity Project (1)/Assets/Scripts/CheckGround.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    private PlayerController player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = true;
+        }
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = false;
+        }
+    }
+}
--- New Unity Project (1)/Assets/Scripts/PlayerController.cs
+++ New Unity Project (1)/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float maxSpeed = 5f;
+    public float speed = 2f;
+    public bool grounded;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+    public float JumpSonic = 6.5f;
+    private bool JumpD;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+        anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+        anim.SetBool("Grounded", grounded);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            JumpD = true;
+        }
+    }   
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb2d.AddForce(Vector2.right * speed * h);
+
+        float limitedspeed = Mathf.Clamp(rb2d.velocity.x,-maxSpeed,maxSpeed);
+        rb2d.velocity = new Vector2(limitedspeed, rb2d.velocity.y);
+
+        if (h > 0.1f)
+        {
+            transform.localScale = new Vector3(2.5f, 2.5f, 1f);
+        }
+        if (h < -0.1f)
+        {
+            transform.localScale = new Vector3(-2.5f, 2.5f, 1f);
+        }
+        if (Mathf.Abs(rb2d.velocity.x) <=maxSpeed*0.9)
+        {
+            rb2d.velocity = new Vector2(0f, rb2d.velocity.y);
+        }
+        if (JumpD)
+        {
+            rb2d.AddForce(Vector2.up * JumpSonic, ForceMode2D.Impulse);
+            JumpD = false;
+        }
+
+
+
+        Debug.Log(rb2d.velocity.x);
     }
 }
