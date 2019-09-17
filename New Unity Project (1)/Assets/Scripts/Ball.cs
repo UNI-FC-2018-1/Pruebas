2a6949e937cdb5aca8f84da446e50efcc3689033
Mordecai <edwincesaryauyosoto@gmail.com>
2019-09-17 10:30:13 -0500

    Lanzamiento de pelota
    Habilidad especial agregada y mejora en el movimient.

---
+++ New Unity Project (1)/Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    
+    public bool BallLaunch;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("BallLaunch", BallLaunch);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            gameObject.SetActive(false);
+            transform.position = new Vector2(-4.58f, 4.8f);
+         
+            
+
+        }
+    }
+}
--- New Unity Project (1)/Assets/Scripts/PlayerController.cs
+++ New Unity Project (1)/Assets/Scripts/PlayerController.cs
@@ -4,36 +4,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject Player;
+    public GameObject Ball;
+    
     public float maxSpeed = 5f;
     public float speed = 2f;
     public bool grounded;
+    public Vector3 velocidad;
+    public Vector3 BallPos;
     private Rigidbody2D rb2d;
     private Animator anim;
     public float JumpSonic = 6.5f;
+    public float ballSpeed = 6.5f;
+    public float Posy;
     private bool JumpD;
     public bool Jump;
     private bool InAir;
-    private bool Fall;
-    // Start is called before the first frame update
+    private bool LaunchD;
+    private bool Launch;
+    
+
+
+    // Start is called before the first frame update    
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-
+       
     }
     void Update()
     {
         anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
         anim.SetBool("Grounded", grounded);
         anim.SetBool("Jump", Jump);
-        anim.SetBool("InAir", InAir);
-        anim.SetBool("Fall", Fall);
+        anim.SetBool("InAir", InAir);   
+        anim.SetBool("Launch", Launch);
+        
+
+
         if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
         {
             JumpD = true;
             
         }
 
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Launch = true;
+            LaunchD = true;
+            
+        }
         
         
   
@@ -44,7 +64,7 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         float h = Input.GetAxis("Horizontal");
-
+        
         rb2d.AddForce(Vector2.right * speed * h);
 
         float limitedspeed = Mathf.Clamp(rb2d.velocity.x,-maxSpeed,maxSpeed);
@@ -53,10 +73,14 @@ public class PlayerController : MonoBehaviour
         if (h > 0.1f)
         {
             transform.localScale = new Vector3(2.5f, 2.5f, 1f);
+            
+            
         }
         if (h < -0.1f)
         {
             transform.localScale = new Vector3(-2.5f, 2.5f, 1f);
+            
+            
         }
         if (Mathf.Abs(rb2d.velocity.x) <=maxSpeed*0.9)
         {
@@ -65,36 +89,54 @@ public class PlayerController : MonoBehaviour
         if (JumpD)
         {
             rb2d.AddForce(Vector2.up * JumpSonic, ForceMode2D.Impulse);
+            Jump = true;
             
+            Debug.Log("Impulso");
             JumpD = false;
            
         }
-        Debug.Log(rb2d.velocity.x);
+
+        if (transform.localScale == new Vector3(2.5f, 2.5f, 1f))
+        {
+            Ball.transform.localScale = new Vector3(2f, 2f, 1f);
+            velocidad = new Vector3(ballSpeed, 0f,0f);
+            BallPos = Player.transform.position + new Vector3(0.87f, 0.27f, 0f);
+        }
+        else if (transform.localScale == new Vector3(-2.5f, 2.5f, 1f))
+        {
+            Ball.transform.localScale = new Vector3(-2f, 2f, 1f);
+            velocidad = new Vector3(-ballSpeed,0f,0f);
+            BallPos = Player.transform.position + new Vector3(-0.87f, 0.27f, 0f);
+        }
+
+
+        if (LaunchD)
+        {
+            Ball.gameObject.SetActive(true);
+            Ball.transform.position = BallPos; //Player.transform.position + new Vector3(0.87f, 0.27f, 0f);
+            Ball.GetComponent<Rigidbody2D>().velocity = velocidad;
+           
+            LaunchD = false;
+            
+            Debug.Log("Lanzamiento");
+        }
 
         
-
-
-    }
-
-   
-
-    /*void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.CompareTag("LimitCol"))
+            if (rb2d.velocity.y < 0)
         {
             Jump = false;
-            InAir = true;
-            Debug.Log("Salió del triger");
-        }
-    }*/
 
-    /*private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("LimitCol"))
-        {
-            InAir = false;
+            InAir = true;
+           
         }
-    }*/
+
+        else InAir = false;
+    }
+
+    
+
+
+
 
 
 
